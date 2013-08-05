a38c5ef36099e24ad479cd46235d766c930fc1ee
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-05 15:25:49 -0500

    adding some grandma logic

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,4 +6,17 @@ end
 
 post '/grandma' do
   "Implement the /grandma route yourself.<br>Params: <code>#{params.inspect}</code>"
+   
+  def grandma_listens something_said
+    
+    if something_said == ""
+      stop_talking if say_something == ""
+    elsif something_said == something_said.upcase
+      puts "NO, NOT SINCE 1983!"
+    elsif something_said == "I love ya, Grandma, but I've got to go."
+      stop_talking
+    else 
+      puts "HUH?! SPEAK UP SONNY!"  
+    end
+  end
 end
